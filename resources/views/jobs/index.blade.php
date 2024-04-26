af4ec378b45bfa80467b29c88d9b0d4b031fb323
Nastya Zavizion <nastya.gorobets95@gmail.com>
2024-04-26 17:45:45 +0300

    Tag component and Job info

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -1,5 +1,26 @@
 <x-layout>
    @foreach($jobs as $job)
-       <x-card class="mb-2">{{$job->title}}</x-card>
+       <x-card class="mb-2">
+           <div class="flex justify-between">
+              <h2 class="text-lg font-medium">
+                  {{$job->title}}
+              </h2>
+               <div class="text-slate-500">$ {{number_format($job->salary)}}</div>
+           </div>
+
+           <div class="mb-4 flex justify-between text-sm text-slate-500">
+               <div class="flex gap-2">
+                   <div>Company name</div>
+                   <div>{{$job->location}}</div>
+               </div>
+
+               <div class="flex gap-2">
+                   <x-tag>{{ucfirst($job->experience)}}</x-tag>
+                   <x-tag>{{$job->category}}</x-tag>
+               </div>
+           </div>
+
+           <p class="text-sm text-slate-500">{{nl2br($job->description)}}</p>
+       </x-card>
     @endforeach
 </x-layout>
