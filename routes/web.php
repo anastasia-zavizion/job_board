1b876687a106ffdaaf4e93441c3314155af04d96
Nastya Zavizion <nastya.gorobets95@gmail.com>
2024-04-26 16:55:34 +0300

    Added main layout component

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('jobs.index');
 });
+
+Route::resource('jobs',\App\Http\Controllers\JobController::class);
